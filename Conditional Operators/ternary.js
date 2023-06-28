8a3788b45dd909f0c6e1fdf0cb91496da06795ca
Aduben <erenabenti@gmail.com>
2023-06-27 21:09:03 -0400

    add ternary operator example

---
+++ Conditional Operators/ternary.js
@@ -0,0 +1,3 @@
+const sales = 300;
+const commissionAmt = sales >300? `You are entitled for ${sales *.02}`:"You are not entitled for any commission";
+console.log(commissionAmt)
---
+++ Functions/functions.js
@@ -0,0 +1,21 @@
+// function ourClass(){
+//     console.log("Inside funciton")
+// }
+// ourClass()
+
+// console.log("done")
+
+// function ourClass(name,face){
+//     console.log(name,face)
+// }
+// ourClass("Adu")
+// console.log("middle")
+// ourClass("kiru")
+// ourClass()
+
+//funciton can be called even before it declaration
+
+ourClass("Adu")
+function ourClass(name,face){
+    console.log(name,face)
+}
