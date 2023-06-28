b459d822ce86ea88db3e98c7684866c3c5f0cab0
Aduben <erenabenti@gmail.com>
2023-06-27 23:55:08 -0400

    #3 Compound interest related ass is submitted

---
+++ Assignments/Saving.js
@@ -0,0 +1,14 @@
+// Prompt the user for initial amount, annual interest rate, and number of years to compound
+const prompt = require("prompt-sync")();
+let principal = parseFloat(prompt("Enter the initial amount in the savings account:"));
+let annualInterestRate = parseFloat(prompt("Enter the annual interest rate (in percentage):"));
+let noYearsToCompound = parseInt(prompt("Enter the number of years to compound:"));
+
+// calculate monthly interest rate by cnverting the interest rate to decimal form 
+let monthlyInterestRate_frac = annualInterestRate / 100 / 12 ;
+
+// Calculate the balance of your saving after the given number of years of compounding
+let balance = principal * Math.pow(1 + monthlyInterestRate_frac, noYearsToCompound * 12);
+
+// Print the balance of saving and round the decimals to three digit.
+console.log(`The Balance of your saving after", ${noYearsToCompound}, "years is:", ${balance.toFixed(3)}`) 
