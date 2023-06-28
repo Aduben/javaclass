a8d173e53fb57c6727bb0b22958ec9d14ba283fb
Aduben <erenabenti@gmail.com>
2023-06-27 21:12:05 -0400

    Add ternary operator example

--- Conditional Operators/ternary.js
+++ Conditional Operators/ternary.js
@@ -1,3 +1,4 @@
+//ternary operator is used as alternative to if-else statments when you need to choose between two expressions
 const sales = 300;
 const commissionAmt = sales >300? `You are entitled for ${sales *.02}`:"You are not entitled for any commission";
-console.log(commissionAmt)
+console.log(commissionAmt);    
